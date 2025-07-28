5f82a95c441fb2aa88861b4ee00efc97560b3c62
lvcs <22395524+lllvcs@users.noreply.github.com>
2025-07-29 02:59:47 +0800

    Update license and fix the name of the package.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='CRISGI',
+    name='crisgi',
     version='0.1.0',
     author='Lyu C., Jiang A., Ng K. H., Liu X., Chen L.',
     author_email='',
